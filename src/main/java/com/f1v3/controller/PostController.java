85256326e28e9888acaac5d3add6ee136521b95e
f1v3-dev <f1v3@kakao.com>
2024-04-29 23:34:55 +0900

    feat: 기본 controller 생성 및 테스트

--- src/main/java/com/f1v3/controller/PostController.java
+++ src/main/java/com/f1v3/controller/PostController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PostController {
 
+
+    /**
+     * 기본 메서드
+     * @return status : 200, body : "Hello f1v3"
+     */
     @GetMapping("/posts")
     public String get() {
         return "Hello f1v3";
