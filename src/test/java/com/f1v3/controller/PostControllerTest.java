91c9638837c19bca0bea7b3a44ab9be18bf7ffbf
f1v3-dev <f1v3@kakao.com>
2024-05-01 01:55:56 +0900

    feat: Post Create Test 추가

--- src/test/java/com/f1v3/controller/PostControllerTest.java
+++ src/test/java/com/f1v3/controller/PostControllerTest.java
@@ -1,38 +1,44 @@
 package com.f1v3.controller;
 
+import com.f1v3.domain.Post;
+import com.f1v3.repository.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(PostController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class PostControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
-    @Test
-    @DisplayName("/f1v3 요청 시 'Hello f1v3'를 반환")
-    void getF1v3() throws Exception {
+    @Autowired
+    PostRepository postRepository;
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/f1v3"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("Hello f1v3"))
-                .andDo(print());
+    @BeforeEach
+    void clear() {
+        postRepository.deleteAll();
     }
 
+
     @Test
-    @DisplayName("/posts 요청시 Hello World를 출력한다.")
+    @DisplayName("/posts 요청시 title, content 값이 정상이면 {}, 200 반환")
     void test() throws Exception {
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
+        mockMvc.perform(post("/posts")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"title\":\"제목입니다.\",\"content\":\"내용입니다.\"}"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -45,7 +51,7 @@ class PostControllerTest {
     @DisplayName("/posts 요청시 title 값은 필수다.")
     void test2() throws Exception {
 
-        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
+        mockMvc.perform(post("/posts")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content("{\"title\":null,\"content\":\"내용입니다.\"}"))
                 .andExpect(MockMvcResultMatchers.status().isBadRequest())
@@ -53,5 +59,24 @@ class PostControllerTest {
                 .andExpect(jsonPath("$.message").value("잘못된 요청입니다."))
                 .andExpect(jsonPath("$.validation.title").value("title은 필수 값입니다."))
                 .andDo(print());
+    }
+
+    @Test
+    @DisplayName("/posts 요청시 DB에 값이 저장된다.")
+    void test3() throws Exception {
+
+        // when
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\":\"제목입니다.\",\"content\":\"내용입니다.\"}"))
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        // then
+        assertEquals(1L, postRepository.count());
+
+        Post post = postRepository.findAll().get(0);
+        assertEquals("제목입니다.", post.getTitle());
+        assertEquals("내용입니다.", post.getContent());
     }
 }
