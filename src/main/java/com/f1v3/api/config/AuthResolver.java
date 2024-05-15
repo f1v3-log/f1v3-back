eedacf5e3b7d53dd9230240df10612a6cf569004
f1v3-dev <f1v3@kakao.com>
2024-05-15 14:51:46 +0900

    feat: Cookie를 사용한 인증

--- src/main/java/com/f1v3/api/config/AuthResolver.java
+++ src/main/java/com/f1v3/api/config/AuthResolver.java
@@ -4,14 +4,20 @@ import com.f1v3.api.config.data.UserSession;
 import com.f1v3.api.domain.Session;
 import com.f1v3.api.exception.Unauthorized;
 import com.f1v3.api.repository.SessionRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.util.Strings;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
+import java.util.Arrays;
+import java.util.Objects;
+
+@Slf4j
 @RequiredArgsConstructor
 public class AuthResolver implements HandlerMethodArgumentResolver {
 
@@ -24,10 +30,25 @@ public class AuthResolver implements HandlerMethodArgumentResolver {
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        String accessToken = webRequest.getHeader("Authorization");
-        if (Strings.isEmpty(accessToken)) {
+        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+
+        // @Nullable -> check
+        if (Objects.isNull(servletRequest)) {
+            log.error(">> servletRequest is null.");
             throw new Unauthorized();
         }
+
+        Cookie[] cookies = servletRequest.getCookies();
+        if (cookies.length == 0) {
+            log.error(">> cookie is empty.");
+            throw new Unauthorized();
+        }
+
+        String accessToken = Arrays.stream(cookies)
+                .filter(cookie -> "SESSION".equals(cookie.getName()))
+                .findFirst()
+                .orElseThrow(Unauthorized::new)
+                .getValue();
 
         // DB 사용자 확인 작업
         Session session = sessionRepository.findByAccessToken(accessToken)
--- src/main/java/com/f1v3/api/controller/AuthController.java
+++ src/main/java/com/f1v3/api/controller/AuthController.java
@@ -1,13 +1,17 @@
 package com.f1v3.api.controller;
 
 import com.f1v3.api.request.Login;
-import com.f1v3.api.response.SessionResponse;
 import com.f1v3.api.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
 
 @Slf4j
 @RestController
@@ -17,8 +21,22 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/auth/login")
-    public SessionResponse login(@RequestBody Login login) {
+    public ResponseEntity<Object> login(@RequestBody Login login) {
         String accessToken = authService.signIn(login);
-        return new SessionResponse(accessToken);
+
+        ResponseCookie cookie = ResponseCookie.from("SESSION", accessToken)
+                .domain("localhost") // TODO: Profile 별로 분리해보기!
+                .path("/")
+                .httpOnly(true)
+                .secure(false)
+                .maxAge(Duration.ofDays(30))
+                .sameSite("Strict")
+                .build();
+
+        log.info(">> cookie = {}", cookie);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .build();
     }
 }
