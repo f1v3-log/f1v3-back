40689bd4878adda31bf41e8d4358889a1d37ad42
f1v3-dev <f1v3@kakao.com>
2024-07-02 17:12:51 +0900

    feat: 댓글 - javadoc 작성

--- src/main/java/com/f1v3/api/controller/CommentController.java
+++ src/main/java/com/f1v3/api/controller/CommentController.java
@@ -16,6 +16,12 @@ public class CommentController {
 
     private final CommentService commentService;
 
+    /**
+     * 댓글 작성 메서드.
+     * @param postId 게시글 ID
+     * @param request 댓글 작성 요청 객체 (작성자, 비밀번호, 내용)
+     *
+     */
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/posts/{postId}/comments")
     public void write(@PathVariable Long postId,
@@ -24,6 +30,11 @@ public class CommentController {
         commentService.write(postId, request);
     }
 
+    /**
+     * 댓글 삭제 메서드.
+     * @param commentId 댓글 ID
+     * @param request 댓글 삭제 요청 객체 (비밀번호)
+     */
     @PostMapping("/comments/{commentId}/delete")
     public void delete(@PathVariable Long commentId,
                        @RequestBody @Valid CommentDelete request) {
